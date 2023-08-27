0d6ad0cd6c0f7433abd8a97c7cc70c9a480b3407
prady8339 <76277136+prady8339@users.noreply.github.com>
2023-08-27 07:03:09 +0530

    top 5 location:

--- py-app/demo.py
+++ py-app/demo.py
@@ -1,4 +1,5 @@
 import folium
+import math
 import geopandas
 import pandas as pd
 import streamlit as st
@@ -6,15 +7,6 @@ import geopandas as gpd
 from shapely.geometry import Point
 from streamlit_folium import st_folium
 
-# import requests
-# import geopandas as gpd
-# from io import StringIO
-
-# url = 'https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson'
-# response = requests.get(url)
-# data = StringIO(response.text)
-
-# world = gpd.read_file(data)
 
 def init_map(center=[22.6139, 85.2090], zoom_start=4.5, map_type="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png", max_zoom=5):
     attr = "<a href='https://stadiamaps.com/'>Stadia Maps</a>"
@@ -22,7 +14,17 @@ def init_map(center=[22.6139, 85.2090], zoom_start=4.5, map_type="https://tiles.
 
 init_map()
 
+def state_prediction():
+    st.write("hello")
 
+def calculate_distance(lat1, long1, lat2, long2):
+    pi = 3.14159265358979323846
+    dist = math.sin(math.radians(lat1)) * math.sin(math.radians(lat2)) + \
+           math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * \
+           math.cos(math.radians(long1 - long2))
+    dist = math.acos(dist)
+    dist = (6371 * pi * dist) / 180
+    return dist
 
 
 def create_point_map(df):
@@ -45,23 +47,68 @@ def plot_from_df(df, folium_map):
                       opacity=row.Opacity,
                       icon=icon).add_to(folium_map)
     return folium_map
-# def plot_from_df(df, folium_map):
-#     df = create_point_map(df)
-#     circle_options = {
-#         'color': 'blue',  # Set circle outline color to blue
-#         'fill_color': 'blue',  # Set fill color to blue
-#         'fill_opacity': 0.5,  # Set fill opacity
-#     }
-#     for i, row in df.iterrows():
-#         radius = row.Icon_Size / 2  # Calculate radius from icon size
-#         folium.CircleMarker(
-#             location=[row.Latitude, row.Longitude],
-#             radius=radius,
-#             tooltip=f'{row.ID}',
-#             **circle_options
-#         ).add_to(folium_map)
-#     return folium_map
+    
+def plot_from_Citydf(df, folium_map):
+    df = create_point_map(df)
+    for i, row in df.iterrows():
+        radius = 5
+        folium.CircleMarker(
+            location=[row.Latitude, row.Longitude],
+            popup="potential walmart location",
+            radius=radius,
+            color='white',
+            fill=True,
+            fill_color='skyblue',
+            fill_opacity=0.8,
+            tooltip=f'{row.ID}'
+        ).add_to(folium_map)
+    return folium_map
 
+def clear_map_markers(folium_map):
+    st.cache_resource.clear()
+    children_copy = folium_map._children.copy()  # Create a copy of the dictionary
+    for layer_key, layer in children_copy.items():
+        if isinstance(layer, (folium.Marker, folium.CircleMarker)):
+            folium_map._children.pop(layer_key)  # Remove the marker from the map's dictionary
+    return folium_map
+
+
+
+def get_point_from_state():
+    cityData = load_Citydf()
+    cityData[['Latitude', 'Longitude']] = cityData[['Latitude', 'Longitude']].apply(pd.to_numeric, errors='coerce')
+    cityData['coordinates'] = cityData[['Latitude', 'Longitude']].values.tolist()
+    cityData['coordinates'] = cityData['coordinates'].apply(Point)
+    cityData = geopandas.GeoDataFrame(cityData, geometry='coordinates')
+    cityData = cityData.dropna(subset=['Latitude', 'Longitude', 'coordinates'])
+    if st.session_state.state_select:
+        cityData = cityData[cityData['State'].isin(st.session_state.state_select)]
+    
+    return cityData
+
+
+def calculate_distance(lat1, long1, lat2, long2):
+    pi = 3.14159265358979323846
+    dist = math.sin(math.radians(lat1)) * math.sin(math.radians(lat2)) + \
+           math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * \
+           math.cos(math.radians(long1 - long2))
+    dist = math.acos(dist)
+    dist = (6371 * pi * dist) / 180
+    return dist
+
+def select_top_5():
+    walmartData = load_df()
+    cityData = get_point_from_state()
+    cityData['min_distance'] = float('inf')  # Initialize minimum distance column
+    for walmart_index, walmart_row in walmartData.iterrows():
+        walmart_lat = walmart_row['Latitude']
+        walmart_lon = walmart_row['Longitude']
+        cityData['distance'] = cityData.apply(lambda row: calculate_distance(row['Latitude'], row['Longitude'], walmart_lat, walmart_lon), axis=1)
+        cityData['min_distance'] = cityData[['min_distance', 'distance']].min(axis=1)  # Update minimum distance column
+    cityData = cityData.sort_values(by=['min_distance'])
+    cityData = cityData.head(5)
+    print(cityData)
+    return cityData
 
 def load_df():
     #cityData = pd.read_csv('../data/cityData.csv')
@@ -73,6 +120,11 @@ def load_df():
     #         'Latitude': [28.5275544,19.082502],
     #         'Longitude': [77.0441742,72.7163741]}
     df = pd.DataFrame(walmartStores)
+    return df
+
+def load_Citydf():
+    cityData = pd.read_csv('../data/cityData.csv')
+    df = pd.DataFrame(cityData)
     return df
 
 
@@ -99,10 +151,8 @@ IM_CONSTANTS = {'LOGO': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ
                 0: 'https://uploads-ssl.webflow.com/64248e7fd5f30d79c9e57d64/64e6177329c2d71389b1b219_walmart.png',
                 1: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQorPbeKx3qEC0IMzcqIdCQeyJuf929raImVcPKSWU&s',
                 }
-SELECTED_MAP = {'walmart potential location': 0, 'walmart potential location2': 1}
+SELECTED_MAP = {'walmart store': 0, 'walmart store': 1}
 
-# world = gpd.read_file('countries.geojson')
-# india = world[world['ADMIN'] == 'India']
 
 @st.cache_resource  # @st.cache_data
 def load_map():
@@ -110,17 +160,13 @@ def load_map():
     m = init_map()  # init
     df = load_df()  # load data
     m = plot_from_df(df, m)  # plot points
-    # folium.GeoJson(india, style_function=lambda feature: {
-    # 'fillColor': '#0000FF',  # Blue color for India
-    # 'color': 'black',
-    # 'weight': 2,
-    # 'dashArray': '5, 5',}).add_to(m)
-    # folium.GeoJson(world, style_function=lambda feature: {
-    #         'fillColor': '#FFFFFF',  # White color for other countries
-    #         'color': 'black',
-    #         'weight': 2,
-    #         'dashArray': '5, 5',}).add_to(m)
     return m
+
+def add_to_map(m):
+    cityData = select_top_5()
+    m = plot_from_Citydf(cityData, m)
+    return m
+
 
     
 
@@ -136,6 +182,7 @@ def main():
         st.session_state.road_quality_factor = 0
     if 'economy_factor' not in st.session_state:
         st.session_state.economy_factor = 0
+    
 
     state_names = [
         "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh",
@@ -148,16 +195,29 @@ def main():
         "Daman and Diu", "Delhi", "Lakshadweep", "Puducherry"
     ]
 
-    st.sidebar.multiselect('Select State', state_names, st.session_state.state_names)
-    st.sidebar.slider('Population Factor', 0, 100, st.session_state.population_factor)
-    st.sidebar.slider('Road Quality Factor', 0, 100, st.session_state.road_quality_factor)
-    st.sidebar.slider('Economy Factor', 0, 100, st.session_state.economy_factor)
+    st.sidebar.multiselect('Select State', state_names, key="state_select")
+    st.sidebar.slider('Population Factor', 0, 100, key="population_factor")
+    st.sidebar.slider('Road Quality Factor', 0, 100, key="road_quality_factor")
+    st.sidebar.slider('Economy Factor', 0, 100, key="economy_factor")
+    #print(st.session_state)
+   
 
-    if st.sidebar.button('Apply'):
-        st.session_state.state_names = st.sidebar.multiselect('Select State', state_names)
-        st.session_state.population_factor = st.sidebar.slider('Population Factor', 0, 100)
-        st.session_state.road_quality_factor = st.sidebar.slider('Road Quality Factor', 0, 100)
-        st.session_state.economy_factor = st.sidebar.slider('Economy Factor', 0, 100)
+
+
+        
+    # load map data @st.cache_resource
+    
+    m = load_map() 
+      
+    def clearInput():
+        for key in list(st.session_state.keys()):  # Using list to avoid RuntimeError due to change in dict size during iteration
+            if key != '8d3a683caaa5100ea90e5eb8746e1c21f3fd3aee816274287029aaf969f69332':
+                del st.session_state[key]
+
+    #st.sidebar.button('Clear',on_click=clearInput())
+    st.sidebar.button('Apply', on_click=state_prediction)
+    st.sidebar.button('Suggest', on_click=add_to_map, args=(m,))
+    st.sidebar.button('Clear', on_click=clear_map_markers, args=(m,))
 
 
 
@@ -171,53 +231,15 @@ def main():
                     }
             </style>
             """, unsafe_allow_html=True)
-    #st.title(TITLE)
 
-    # load map data @st.cache_resource
-    m = load_map()
+
+  
     # init stored values
     if "selected_id" not in st.session_state:
         st.session_state.selected_id = None
 
-    # setting up the header with its own row
-    # _, r1_col1, r1_col2, r1_col3, _ = st.columns([1, 4.5, 1, 6, 1])
-    # with r1_col1:
-    #     st.markdown(f"<p style='font-size: 27px;'><i>Interactive Mapping Demonstration</i></p>",
-    #                 unsafe_allow_html=True)
-    # with r1_col3:
-    #     st.write('')
-
-    # main information line: includes map location
     map_col = st.columns([1])
-    # _, r2_col1, r2_col2, r2_col3, _ = st.columns([1, 4.5, 1, 6, 1])
-    # with r2_col1:
-        # info sidebar
-        # r2_col1.markdown('## Next Best Location')
-        # text1, text2 = "stat1", "stat2"
-       # st.markdown(FACT_BACKGROUND.format(text1, IM_CONSTANTS[0], 24, text2), unsafe_allow_html=True)
-        # st.markdown("""<div style="padding-top: 15px;"></div>""", unsafe_allow_html=True)
-        # text1, text2 = "locations", " YY "
-        #st.markdown(FACT_BACKGROUND.format(text1, IM_CONSTANTS[1], 30, text2), unsafe_allow_html=True)
-        
-        # st.multiselect('Select State', state_names)
-        # st.slider('Population Factor', 0, 100)
-        # st.slider('Road Quality Factor', 0, 100)
-        # st.slider('Economy Factor', 0, 100)
-        # st.button('Apply')
-        # white space
-        # for _ in range(10):
-        #     st.markdown("")
-
-        # place for logos
-        # logo1, logo2, _ = st.columns([1, 1, 2])
-        #logo1.image(IM_CONSTANTS['LOGO'], width=110)
-
-    # white space
-    # with r2_col2:
-    #     st.write("")
-
-    # map container
-    # with r2_col3:
+  
     _,map_col,_ = st.columns([1,10,1])
     with map_col:
         level1_map_data = st_folium(m, height=700, width=1024)
@@ -231,33 +253,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-# st.button('Click me')
-# st.data_editor('Edit data', data)
-# st.checkbox('I agree')
-# st.toggle('Enable')
-# st.radio('Pick one', ['cats', 'dogs'])
-# st.selectbox('Pick one', ['cats', 'dogs'])
-# st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
-# st.slider('Pick a number', 0, 100)
-# st.select_slider('Pick a size', ['S', 'M', 'L'])
-# st.text_input('First name')
-# st.number_input('Pick a number', 0, 10)
-# st.text_area('Text to translate')
-# st.date_input('Your birthday')
-# st.time_input('Meeting time')
-# st.file_uploader('Upload a CSV')
-# st.download_button('Download file', data)
-# st.camera_input("Take a picture")
-# st.color_picker('Pick a color')
-
-# # Use widgets' returned values in variables:
-# >>> for i in range(int(st.number_input('Num:'))):
-# >>>   foo()
-# >>> if st.sidebar.selectbox('I:',['f']) == 'f':
-# >>>   b()
-# >>> my_slider_val = st.slider('Quinn Mallory', 1, 88)
-# >>> st.write(slider_val)
-
-# # Disable widgets to remove interactivity:
-# >>> st.slider('Pick a number', 0, 100, disabled=True)
